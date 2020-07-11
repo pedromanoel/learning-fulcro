1b91c7e8913bbbf92b99b42045ce80bec7c1807a
Pedro Evangelista <pedro.evangelista@gmail.com>
2020-07-11 19:33:32 -0300

    Add example with a data-tree and a ui-tree
    The UI is organized as a tree of elements, and the data matches
    the tree
    
    UI
    ---
    [Root]
     |---[PersonList]
          |---[Person]
          \---...
     \---[PersonList]
          |---[Person]
          \---...
    DATA
    ---
    [ui-data]
     |---[:friends]
          |---[:person]
          \---...
     \---[:enemies]
          |---[:person]
          \---...

--- src/main/app/ui.cljs
+++ src/main/app/ui.cljs
@@ -4,14 +4,27 @@
     [com.fulcrologic.fulcro.dom :as dom]))
 
 (defsc Person [this {:person/keys [name age]}]
-  (dom/div
-    (dom/p "Name: " name)
-    (dom/p "Age: " age)))
+  (dom/li
+    (dom/h5 (str name " (age: " age ")"))))
 
-(def ui-person (comp/factory Person))
+;; The keyfn generates a react key for each element based on props. See React documentation on keys.
+(def ui-person (comp/factory Person {:keyfn :person/name}))
+
+(defsc PersonList [this {:list/keys [label people]}]
+  (dom/div
+    (dom/h4 label)
+    (dom/ul
+      (map ui-person people))))
+
+(def ui-person-list (comp/factory PersonList))
 
 (defsc Root [this props]
-  (dom/div
-    (dom/h1 "People")
-    (ui-person #:person{:name "Pedro Manoel" :age 35})
-    (ui-person #:person{:name "Débora Miglino Evangelista" :age 34})))
+  (let [ui-data {:friends #:list{:label "Friends"
+                                 :people [#:person{:name "Débora" :age 35}
+                                          #:person{:name "Cecília" :age 2}
+                                          #:person{:name "Fabrício" :age 0}]}
+                 :enemies #:list{:label "Enemies"
+                                 :people [#:person{:name "Bozo" :age 56}]}}]
+    (dom/div
+      (ui-person-list (:friends ui-data))
+      (ui-person-list (:enemies ui-data)))))
