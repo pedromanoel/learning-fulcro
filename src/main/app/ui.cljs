8477087ee64e82ad5842d6e5013f351fdb11f353
Pedro Evangelista <pedro.evangelista@gmail.com>
2020-07-11 21:54:59 -0300

    Move data closer to the components

--- src/main/app/ui.cljs
+++ src/main/app/ui.cljs
@@ -4,6 +4,9 @@
     [com.fulcrologic.fulcro.dom :as dom]))
 
 (defsc Person [this {:person/keys [name age]}]
+  {:initial-state
+   (fn [{:keys [name age]}]
+     #:person{:name name :age age})}
   (dom/li
     (dom/h5 (str name " (age: " age ")"))))
 
@@ -11,6 +14,17 @@
 (def ui-person (comp/factory Person {:keyfn :person/name}))
 
 (defsc PersonList [this {:list/keys [label people]}]
+  {:initial-state
+   (fn [{:keys [label]}]
+     #:list{:label  label
+            :people (case label
+                      "Friends"
+                      [(comp/get-initial-state Person {:name "Débora" :age 35})
+                       (comp/get-initial-state Person {:name "Cecília" :age 2})
+                       (comp/get-initial-state Person {:name "Fabrício" :age 0})]
+                      "Enemies"
+                      [(comp/get-initial-state Person {:name "Bozo" :age 56})]
+                      [])})}
   (dom/div
     (dom/h4 label)
     (dom/ul
@@ -18,13 +32,10 @@
 
 (def ui-person-list (comp/factory PersonList))
 
-(defsc Root [this props]
-  (let [ui-data {:friends #:list{:label "Friends"
-                                 :people [#:person{:name "Débora" :age 35}
-                                          #:person{:name "Cecília" :age 2}
-                                          #:person{:name "Fabrício" :age 0}]}
-                 :enemies #:list{:label "Enemies"
-                                 :people [#:person{:name "Bozo" :age 56}]}}]
-    (dom/div
-      (ui-person-list (:friends ui-data))
-      (ui-person-list (:enemies ui-data)))))
+(defsc Root [this {:keys [friends enemies]}]
+  {:initial-state
+   (fn [_] {:friends (comp/get-initial-state PersonList {:label "Friends"})
+            :enemies (comp/get-initial-state PersonList {:label "Enemies"})})}
+  (dom/div
+    (ui-person-list friends)
+    (ui-person-list enemies)))
