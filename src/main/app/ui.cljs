7cd8c1bf7e89835e48b66e6457389e56fbbc2602
Pedro Evangelista <pedro.evangelista@gmail.com>
2020-07-11 22:36:45 -0300

    Add callback from parent

--- src/main/app/ui.cljs
+++ src/main/app/ui.cljs
@@ -3,16 +3,21 @@
     [com.fulcrologic.fulcro.components :as comp :refer [defsc]]
     [com.fulcrologic.fulcro.dom :as dom]))
 
-(defsc Person [this {:person/keys [name age]}]
+(defsc Person [this {:person/keys [name age]} {:keys [onDelete]}]
   {:query [:person/name :person/age]
    :initial-state
    (fn [{:keys [name age]}]
      #:person{:name name :age age})}
   (dom/li
-    (dom/h5 (str name " (age: " age ")"))))
+    (dom/h5
+      (str name " (age: " age ")")
+      (dom/button {:onClick #(onDelete name)} "X"))))
 
 ;; The keyfn generates a react key for each element based on props. See React documentation on keys.
 (def ui-person (comp/factory Person {:keyfn :person/name}))
+
+(defn ui-person-with-on-delete [onDelete]
+  (fn [props] (ui-person (comp/computed props {:onDelete onDelete}))))
 
 (defsc PersonList [this {:list/keys [label people]}]
   {:query [:list/label {:list/people (comp/get-query Person)}]
@@ -27,10 +32,11 @@
                       "Enemies"
                       [(comp/get-initial-state Person {:name "Bozo" :age 56})]
                       [])})}
-  (dom/div
-    (dom/h4 label)
-    (dom/ul
-      (map ui-person people))))
+  (let [delete-person (fn [name] (println label "asked to delete" name))]
+    (dom/div
+      (dom/h4 label)
+      (dom/ul
+        (map (ui-person-with-on-delete delete-person) people)))))
 
 (def ui-person-list (comp/factory PersonList))
 
